e76c0b6e2877b324094401340ac920af740d687a
Viviancat666 <vivian9069@gmail.com>
2024-09-27 02:44:21 +0300

    Update solution.js

--- DOM Exercise/06. Format the Text/solution.js
+++ DOM Exercise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  const text = document.querySelector("#input").value;
+  const output = document.querySelector("#output");
+  const sentances = text.split(". ");
+  const paragraphs = Math.ceil(sentances.length / 3);
+
+  for (let i = 0; i < paragraphs; i++) {
+    let currPar = document.createElement("p");
+    currPar.innerHTML = sentances.slice(i * 3, i * 3 + 3).join("\n");
+    output.append(currPar);
+  }
 }
