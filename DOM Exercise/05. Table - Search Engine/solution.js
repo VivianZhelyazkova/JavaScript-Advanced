25d36970002a71e73424b64c3082f95c8dde7dd6
Viviancat666 <vivian9069@gmail.com>
2024-09-27 02:44:30 +0300

    Update solution.js

--- DOM Exercise/05. Table - Search Engine/solution.js
+++ DOM Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,14 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    const search = document.querySelector("#searchField").value;
+    const elements = Array.from(document.querySelectorAll("tbody td"));
+    elements.forEach((el) => el.parentElement.classList.remove("select"));
+    elements.forEach((el) => {
+      if (el.textContent.includes(search)) {
+        el.parentElement.classList.add("select");
+      }
+    });
+  }
 }
