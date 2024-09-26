030413323ff2f195d52f507471b54df4ce94c58c
Viviancat666 <vivian9069@gmail.com>
2024-09-27 02:44:26 +0300

    Update SumTable.js

--- DOM/06.Sum Table/SumTable.js
+++ DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+  let sum = 0;
+  const firstTable = document.querySelector("table");
+  const lastColumns = firstTable.querySelectorAll("td:last-child");
+  let result = Array.from(lastColumns).pop();
+  for (let el of lastColumns) {
+    sum += Number(el.textContent);
+  }
+  result.textContent = sum;
 }
