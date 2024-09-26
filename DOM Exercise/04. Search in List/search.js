65059c7fb87d6f948755d455d75bc92e0a90f450
Viviancat666 <vivian9069@gmail.com>
2024-09-27 02:44:32 +0300

    Update search.js

--- DOM Exercise/04. Search in List/search.js
+++ DOM Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+  const towns = document.querySelectorAll("#towns li");
+  const input = document.querySelector("#searchText").value;
+  const result = document.querySelector("#result");
+  let counter = 0;
+
+  for (let town of towns){
+   town.style.fontWeight = "regular";
+   town.style.textDecoration = "none";
+      if (town.textContent.includes(input)){
+         town.style.fontWeight = "bold";
+         town.style.textDecoration = "underline";
+         counter++;
+      }
+  }   
+  result.textContent = `${counter} matches found`
 }
