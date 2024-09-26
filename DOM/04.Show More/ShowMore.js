e36260f35681501247d32baae797df9b6b48645c
Viviancat666 <vivian9069@gmail.com>
2024-09-27 02:44:15 +0300

    Update ShowMore.js

--- DOM/04.Show More/ShowMore.js
+++ DOM/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+  const readMore = document.getElementById("more");
+  const more = document.getElementById("text");
+  readMore.style.display = "none";
+  more.style.display = "inline";
 }
