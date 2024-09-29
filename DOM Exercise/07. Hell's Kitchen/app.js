cf9d007f2a30d651d33cc99a60f5ebefaf578060
Viviancat666 <vivian9069@gmail.com>
2024-09-29 23:19:27 +0300

    Update app.js

--- DOM Exercise/07. Hell's Kitchen/app.js
+++ DOM Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,29 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector("#btnSend").addEventListener("click", onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  function onClick() {
+    const restaurants = JSON.parse(
+      document.querySelector("#inputs textarea").value
+    );
+    const bestRestaurantNameEl = document.querySelector("#bestRestaurant p");
+    const bestRestaurantWorkersEl = document.querySelector("#workers p");
+
+    const restaurantsDict = {};
+
+    for (let restaurant of restaurants) {
+      const [resName, data] = restaurant.split(" - ");
+      const workers = data.split(", ");
+      if (!(resName in restaurantsDict)) {
+        restaurantsDict[resName] = {};
+      }
+
+      for (let worker of workers) {
+        const [workerName, salary] = worker.split(" ");
+
+        restaurantsDict[resName][workerName] = salary;
+      }
+    }
+    console.log(restaurantsDict);
+    
+  }
 }
