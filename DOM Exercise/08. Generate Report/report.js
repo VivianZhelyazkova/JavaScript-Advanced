f83aa12e0519322cb3da310fb9146faaff9734b2
Viviancat666 <vivian9069@gmail.com>
2024-09-29 23:19:30 +0300

    Update report.js

--- DOM Exercise/08. Generate Report/report.js
+++ DOM Exercise/08. Generate Report/report.js
@@ -1,3 +1,25 @@
 function generateReport() {
-    //TODO
+  const checkboxes = Array.from(
+    document.querySelectorAll('input[type="checkbox"]')
+  );
+  const data = {};
+  for (let checkbox of checkboxes) {
+    if (checkbox.checked) {
+      let index = checkboxes.indexOf(checkbox) + 1;
+      data[checkbox.name] = Array.from(
+        document.querySelectorAll(`tbody td:nth-child(${index})`)
+      ).map((el) => el.textContent);
+    }
+  }
+  const result = [];
+  for (let i = 0; i < data[Object.keys(data)[0]].length; i++) {
+    let currEmployee = {};
+    for (let key in data) {
+      currEmployee[key] = data[key][i];
+    }
+    result.push(currEmployee);
+  }
+
+  const output = document.querySelector("#output");
+  output.value = JSON.stringify(result);
 }
