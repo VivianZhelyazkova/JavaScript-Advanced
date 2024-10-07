816fa99f0bf5891a2fa5b21932aab77e123fd7ed
Viviancat666 <vivian9069@gmail.com>
2024-10-08 02:12:11 +0300

    Update app.js

--- DOM Manipulations/05. Highlight-Active/app.js
+++ DOM Manipulations/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+  const inputs = document.querySelectorAll("div input");
+  inputs.forEach((input) => {
+    input.addEventListener("focus", () => {
+      input.parentElement.classList.add("focused");
+    });
+    input.addEventListener("blur", () => {
+      input.parentElement.classList.remove("focused");
+    });
+  });
 }
