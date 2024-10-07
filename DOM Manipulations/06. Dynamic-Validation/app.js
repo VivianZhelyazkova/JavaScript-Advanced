477d23ff23e25e95e8672252bff5521fceb2f60f
Viviancat666 <vivian9069@gmail.com>
2024-10-08 02:12:02 +0300

    Update app.js

--- DOM Manipulations/06. Dynamic-Validation/app.js
+++ DOM Manipulations/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+  function validate() {
+    const emailInput = document.getElementById("email");
+
+    emailInput.addEventListener("change", () => {
+      const emailPattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+      if (!emailPattern.test(emailInput.value)) {
+        emailInput.classList.add("error");
+      } else {
+        emailInput.classList.remove("error");
+      }
+    });
+  }
 }
