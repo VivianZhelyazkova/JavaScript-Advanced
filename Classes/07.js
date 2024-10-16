0d9b35816ce3a42ca9aab43c0cebf488c35c5cdb
Viviancat666 <vivian9069@gmail.com>
2024-10-16 03:18:27 +0300

    Create 07.js

---
+++ Classes/07.js
@@ -0,0 +1,15 @@
+let myArr = [5, 4, 8];
+
+// for (let i = 0; i <= myArr.length - 1; i++) {
+//     console.log(myArr[i]);
+    
+// }
+
+for (let element of myArr){
+    console.log(element);
+    
+}
+
+// for (let i = myArr.length - 1; i >= 0; i--) {
+//     console.log(myArr[i]);
+// }
